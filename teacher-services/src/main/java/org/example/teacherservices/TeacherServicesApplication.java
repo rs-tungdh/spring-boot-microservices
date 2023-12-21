c4bcb3d47b490b4a533c69d5995af469013caf62
rs-tungdh <tungdh@runsystem.net>
2023-12-21 14:43:52 +0700

    create teacher services

---
+++ teacher-services/src/main/java/org/example/teacherservices/TeacherServicesApplication.java
@@ -0,0 +1,13 @@
+package org.example.teacherservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherServicesApplication.class, args);
+	}
+
+}
---
+++ teacher-services/src/test/java/org/example/teacherservices/TeacherServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teacherservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
